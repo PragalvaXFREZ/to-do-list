d54822953c8c44f40ba8fa16d097ef97b5625b18
PragalvaXFREZ <sapkotapragalva@gmail.com>
2024-09-21 19:51:13 +0545

    my commit

---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+
+function Nav() {
+
+
+  return (
+    <div class="navbar">
+     <ul>
+        <li>Home</li>
+        <li>About</li>
+     </ul>
+     </div>
+  )
+}
+
+export default Nav;
---
+++ src/home.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import Create from './Create';
+import axios from 'axios';
+import { BsCircleFill, BsFillTrashFill, BsFillCheckCircleFill } from 'react-icons/bs';
+import Nav from './nav';
+
+function Home() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/get')
+            .then((result) => {
+                setTodos(result.data);
+            })
+            .catch((error) => {
+                console.error('There was an error fetching the data:', error);
+            });
+    }, []);
+
+    const handleEdit = (id) => {
+        axios.put(`http://localhost:3001/update/${id}`)
+            .then(result => {location.reload()})
+            .catch(error => console.error(error))
+    }
+
+    const handleDelete = (id) => {
+        axios.delete(`http://localhost:3001/delete/${id}`)
+            .then(result => {location.reload()})
+            .catch(error => console.error(error))
+    }
+
+    return (
+        
+        <div className="home">
+            <Nav />
+            <h1>To Do List</h1>
+            <Create />
+            {
+                todos.length === 0
+                    ? <div><h2>No Records Found</h2></div>
+                    : todos.map((todo) => (
+                        <div className='task' key={todo._id}>
+                            <div className='checkbox' onClick={() => handleEdit(todo._id)}>
+                                {todo.done ? <BsFillCheckCircleFill className='icon' /> : <BsCircleFill className='icon' />}
+                               <p className={todo.done ? "line_throuhgh" : ""}>{todo.task}</p> 
+                            </div>
+                            <div>
+                                <span><BsFillTrashFill className='icon' 
+                                onClick={()=> handleDelete(todo._id)}/>
+                                </span>
+                            </div>
+                        </div>
+                    ))
+            }
+        </div>
+    );
+}
+
+export default Home;
